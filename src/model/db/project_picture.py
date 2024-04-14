17fded6aa6c0b390ff513f92e37460813bc40512
SagidKamilov <89615854074a@gmail.com>
2024-04-14 14:33:08 +0300

    Добавил каскадное удаление

--- src/model/db/project_picture.py
+++ src/model/db/project_picture.py
@@ -13,5 +13,5 @@ class ProjectPicture(Base):
     id: Mapped[int] = mapped_column(name="id", primary_key=True, autoincrement=True)
     src_link: Mapped[str] = mapped_column(name="src_link", type_=sqlalchemy.Text, nullable=False)
     type_picture: Mapped[str] = mapped_column(name="type_picture", type_=sqlalchemy.String(length=255))
-    project_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey(Project.id))
+    project_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey(Project.id, ondelete="CASCADE"))
     project: Mapped["Project"] = relationship(back_populates="project_picture")
