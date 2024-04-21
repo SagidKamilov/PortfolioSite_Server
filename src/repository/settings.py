7b98380403b4c890a19ae6975c01bbad2872c575
SagidKamilov <89615854074a@gmail.com>
2024-04-21 23:17:25 +0300

    Настройки над репозиторием

---
+++ src/repository/settings.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from src.config import settings
+
+
+# class AsyncDatabase:
+#     def __init__(self):
+# url = f"postgresql+{settings.DB_POSTGRES_SCHEMA}://{settings.DB_POSTGRES_USERNAME}:{settings.DB_POSTGRES_PASSWORD}@{settings.DB_POSTGRES_HOST}:{settings.DB_POSTGRES_PORT}/{settings.DB_POSTGRES_NAME}"
+url = "postgresql+asyncpg://admin_n01:89ujifd0kl432@localhost:5432/portfolio"
+async_engine = create_async_engine(url=url)
+async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False)
