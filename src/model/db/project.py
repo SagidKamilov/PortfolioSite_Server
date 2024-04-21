17869f3305665a3032c696c37b7687c795b6e3ac
SagidKamilov <89615854074a@gmail.com>
2024-04-21 23:19:22 +0300

    Удалил отношение к таблице проектов. Переименовал поле таблицы

--- src/model/db/project.py
+++ src/model/db/project.py
@@ -6,17 +6,16 @@ from sqlalchemy.sql import functions
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model.db.base import Base
-from src.model.db.project_picture import ProjectPicture
 
 
 class Project(Base):
     __tablename__ = "project"
 
     id: Mapped[int] = mapped_column(name="id", primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(name="title", type=sqlalchemy.String(length=255), nullable=False)
-    description: Mapped[str] = mapped_column(name="description", type=sqlalchemy.Text, nullable=False)
-    create_at: Mapped[datetime.datetime] = mapped_column(name="create_at", type=sqlalchemy.DateTime(timezone=True),
+    title: Mapped[str] = mapped_column(name="title", type_=sqlalchemy.String(length=255), nullable=False)
+    description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text, nullable=False)
+    create_at: Mapped[datetime.datetime] = mapped_column(name="create_at", type_=sqlalchemy.DateTime(timezone=True),
                                                          nullable=False, server_default=functions.now())
-    update_at: Mapped[datetime.datetime] = mapped_column(name="create_at", type=sqlalchemy.DateTime(timezone=True),
-                                                         nullable=False, server_onupdate=sqlalchemy.schema.FetchedValue(for_update=True))
-    project_pictures: Mapped[List["ProjectPicture"]] = relationship(back_populates="project")
+    update_at: Mapped[datetime.datetime] = mapped_column(name="update_at", type_=sqlalchemy.DateTime(timezone=True),
+                                                         server_default=functions.now())
+    # project_pictures: Mapped[List["ProjectPicture"]] = relationship(back_populates="projects")
