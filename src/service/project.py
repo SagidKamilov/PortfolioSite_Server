37797ff4efee881b531f4632e153c0a0579e3c7b
SagidKamilov <89615854074a@gmail.com>
2024-04-26 08:49:49 +0300

    Реализовал проверку полученного объекта сервис

--- src/service/project.py
+++ src/service/project.py
@@ -1,33 +1,43 @@
-from typing import List
+from typing import List, Sequence, Tuple
 
 from src.repository.project import ProjectRepository
 from src.model.db.project import Project
-from src.model.schema.project import ProjectCreatePhotos, ProjectUpdatePhotos, Photos
-from src.exception.database import EntityDoesNotExist
+from src.model.db.picture import ProjectPicture
+from src.model.schema.project import ProjectCreatePicture, ProjectUpdatePictures
 
 
 class ProjectService:
     def __init__(self, project_repo: ProjectRepository):
         self.project_repo = project_repo
 
-    async def create_project(self, project_create: ProjectCreatePhotos) -> Project:
-        project = await self.project_repo.create_project(project_create=project_create)
-        return project
+    async def create_project(self, project_create: ProjectCreatePicture) -> Tuple[Project, Sequence[ProjectPicture]]:
+        project_pictures = await self.project_repo.create_project(project_create=project_create)
+        return project_pictures
 
-    async def get_project(self, project_id: int) -> Project:
-        project = await self.project_repo.get_project_by_id(project_id=project_id)
-        if not project:
-            raise EntityDoesNotExist(f"Проект с id = `{project_id} не найден`")
-        return project
+    async def get_project(self, project_id: int) -> Tuple[Project, Sequence[ProjectPicture]]:
+        project_pictures = await self.project_repo.get_project_by_id(project_id=project_id)
 
-    async def get_all_projects(self) -> List[Project]:
+        if not project_pictures:
+            raise Exception(f"Проект с id = `{project_id} не найден`")
+
+        return project_pictures
+
+    async def get_all_projects(self) -> Sequence[Project]:
         projects = await self.project_repo.get_all_projects()
         return projects
 
-    async def update_project(self, project_id: int, project_update: ProjectUpdatePhotos) -> Project:
-        project = await self.project_repo.update_project(project_id=project_id, project_update=project_update)
-        return project
+    async def update_project(self, project_id: int, project_update: ProjectUpdatePictures) -> Project:
+        project_pictures = await self.project_repo.update_project(project_id=project_id, project_update=project_update)
 
-    async def delete_project(self, project_id: int):
+        if not project_pictures:
+            raise Exception(f"Ошибка обновления! Проекта с id = `{project_id}` не существует!")
+
+        return project_pictures
+
+    async def delete_project(self, project_id: int) -> str:
         result: str = await self.project_repo.delete_project(project_id=project_id)
+
+        if not result:
+            raise Exception(f"Ошибка удаления! Проекта с id = `{project_id}` не существует!")
+
         return result
