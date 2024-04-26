8f0feb4a49a8f501ced5e0a3bffb160df7466e93
SagidKamilov <89615854074a@gmail.com>
2024-04-26 08:18:36 +0300

    Добавил хранение главной и дополнительной картинки в основной таблице project

--- src/model/db/project.py
+++ src/model/db/project.py
@@ -9,13 +9,17 @@ from src.model.db.base import Base
 
 
 class Project(Base):
-    __tablename__ = "project"
+    __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(name="id", primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(name="title", type_=sqlalchemy.String(length=255), nullable=False)
     description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text, nullable=False)
+    main_picture: Mapped[str] = mapped_column(name="main_picture", type_=sqlalchemy.Text, nullable=False)
+    additional_picture: Mapped[str] = mapped_column(name="additional_picture", type_=sqlalchemy.Text, nullable=False)
     create_at: Mapped[datetime.datetime] = mapped_column(name="create_at", type_=sqlalchemy.DateTime(timezone=True),
                                                          nullable=False, server_default=functions.now())
     update_at: Mapped[datetime.datetime] = mapped_column(name="update_at", type_=sqlalchemy.DateTime(timezone=True),
                                                          server_default=functions.now())
-    project_pictures: Mapped[List["ProjectPicture"]] = relationship(back_populates="projects")
+    # account_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey("accounts.id"))
+    # account: Mapped["Account"] = relationship(back_populates="project")
+    project_pictures: Mapped[List["ProjectPicture"]] = relationship(back_populates="project")
